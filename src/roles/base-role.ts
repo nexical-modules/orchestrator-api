d8dc1d7f3e77b8f9a81d339a7d27967791361d10
Adrian Webb <adrian@webb.sh>
2026-02-12 09:13:38 -0500

    Updates to the base-role after updating the generator.

--- src/roles/base-role.ts
+++ src/roles/base-role.ts
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY
 export abstract class BaseRole {
-  public static async check(context: unknown, permission: string): Promise<boolean> {
+  public async check(context: unknown, permission: string): Promise<boolean> {
     return true;
   }
 }
